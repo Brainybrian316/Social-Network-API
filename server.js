259771eacab5430403a7cfd3f6c2a9f79dc77586
Brainybrian316 <brainybrian316@gmail.com>
2022-06-17 13:09:26 -0400

    basic set up completed

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes'));
+// app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
     useNewUrlParser: true,
