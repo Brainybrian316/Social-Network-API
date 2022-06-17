f28d59dbe0800be3ba8c5642dfbd933a338395b1
Brainybrian316 <brainybrian316@gmail.com>
2022-06-17 13:43:28 -0400

    virtual for friend count

--- models/User.js
+++ models/User.js
@@ -33,6 +33,10 @@ const UserSchema = new Schema({
         id: false
     });
 
+    UserSchema.virtual('friendCount').get(function() {
+        return this.friends.length;
+    })
+
     const User = model('User', UserSchema);
 
     module.exports = User;
