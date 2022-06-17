f9333cb86c10d5dec225691f9c679fc718fa08df
Brainybrian316 <brainybrian316@gmail.com>
2022-06-17 13:38:54 -0400

    user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+            }
+    }
+},
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevent virtual from creating dublicate of _id as 'id'
+        id: false
+    });
