dfe6107624e03f49e74d5ec006a163718e489567
Brainybrian316 <brainybrian316@gmail.com>
2022-06-17 15:55:43 -0400

    get all users, one user, and create user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,6 +10,16 @@ const userController = {
             res.status(500).json(err);
         })
 },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate('friends')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
 
     createUser({ body }, res) {
         User.create(body)
